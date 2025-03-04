d27b90b3c20c2bbb1441fe0b4042cce5f299ad6e
cwong10CS <cwong10@zagmail.gonzaga.edu>
2025-03-03 21:46:18 -0800

    Late Submittal: Forgot to push

--- vector.cpp
+++ vector.cpp
@@ -1,12 +1,34 @@
 #include <vector>
+#include <iostream>
+#include <vector>
+
 
 
 void part1() {
-    //TODO: Implement part 1
+    std::vector <int> vector_size;
+
+    for (int i = 0; i < 50000; i++) {
+        vector_size.push_back(i);
+    }
+    std::cout << "After adding elements: Size = " << vector_size.size() << "  Capacity = " << vector_size.capacity() << std::endl;
+    
+    vector_size.resize(vector_size.size()/2);
+    std::cout << "After resizing: Size = " << vector_size.size() << "  Capacity = " << vector_size.capacity() << std::endl;
+
+    vector_size.shrink_to_fit();
+    std::cout << "After shrink_to_fit(): Size = " << vector_size.size() << "  Capacity = " << vector_size.capacity() << std::endl;
+
+    std::cout << std::endl;
 }
 
 void part2() {
-    //TODO: Implement part 2
+
+    std::vector<bool> vector_bool(10000);
+    std::vector<char> vector_char(10000);
+
+    std::cout << "Memory usage of std::vector<bool>: " << vector_bool.capacity() << " bytes" << std::endl;
+    std::cout << "Memory usage of std::vector<char>: " << vector_char.capacity() << " bytes" << std::endl;
+
 }
 
 int main() {
