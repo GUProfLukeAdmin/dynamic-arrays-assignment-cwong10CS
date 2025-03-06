062a3dea8edb886fbf5e5f08c0ade0fe9d8c13a5
GUProfLuke <143226561+GUProfLuke@users.noreply.github.com>
2025-03-06 10:49:45 -0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -26,7 +26,7 @@ void part2() {
     std::vector<bool> vector_bool(10000);
     std::vector<char> vector_char(10000);
 
-    std::cout << "Memory usage of std::vector<bool>: " << vector_bool.capacity() << " bytes" << std::endl;
+    std::cout << "Memory usage of std::vector<bool>: " << vector_bool.capacity()/8 << " bytes" << std::endl;
     std::cout << "Memory usage of std::vector<char>: " << vector_char.capacity() << " bytes" << std::endl;
 
 }
